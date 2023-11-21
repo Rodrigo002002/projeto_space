5f7bb3ab20ef0ab774ab690f4bb10e7639796e4e
rodrigo <esquiloalcoolatra@gmail.com>
2023-11-21 10:10:59 -0300

    projeto space

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse('<h1>Project Space</h1><p>Bem vindo ao espaço</p>')
+    return render(request, 'galeria/index.html')
